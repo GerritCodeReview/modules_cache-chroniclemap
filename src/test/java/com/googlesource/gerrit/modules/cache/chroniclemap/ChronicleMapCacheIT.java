b5e2681a46172328d78708ade51a95fe492d96b2
Antonio Barone <syntonyze@gmail.com>
2020-10-06 15:14:05 +0200

    Prevent gerrit from starting when cache size exceeds diskLimit
    Due to the very nature of chronicle-map persisted file pre-allocation,
    the diskLimit configuration cannot be used to limit the file size on
    disk.
    
    Should the cache file generated by chronicle-map exceed dikLimit, an
    exception will be thrown, preventing Gerrit server from starting.
    
    Bug: Issue 13458
    Change-Id: Id65886dff3f20501d30384066bb6eeada1805d34

--- src/test/java/com/googlesource/gerrit/modules/cache/chroniclemap/ChronicleMapCacheIT.java
+++ src/test/java/com/googlesource/gerrit/modules/cache/chroniclemap/ChronicleMapCacheIT.java
@@ -18,16 +18,32 @@ import static com.google.common.truth.Truth8.assertThat;
 
 import com.google.common.truth.Truth8;
 import com.google.gerrit.acceptance.AbstractDaemonTest;
+import com.google.gerrit.acceptance.GerritServer;
 import com.google.gerrit.acceptance.RestResponse;
+import com.google.gerrit.acceptance.UseLocalDisk;
+import com.google.gerrit.acceptance.config.GerritConfig;
 import com.google.gerrit.entities.Project;
 import com.google.gerrit.extensions.api.accounts.AccountInput;
 import com.google.gerrit.server.cache.PersistentCacheFactory;
+import com.google.gerrit.server.config.SitePaths;
 import com.google.inject.Inject;
+import java.io.File;
 import org.junit.Test;
+import org.junit.runner.Description;
 
 public class ChronicleMapCacheIT extends AbstractDaemonTest {
 
   @Inject PersistentCacheFactory persistentCacheFactory;
+  @Inject SitePaths gerritSitePath;
+  Description testDescription;
+
+  @Override
+  protected void beforeTest(Description description) throws Exception {
+    this.testDescription = description;
+  }
+
+  @Override
+  protected void afterTest() throws Exception {}
 
   @Override
   public com.google.inject.Module createModule() {
@@ -35,12 +51,14 @@ public class ChronicleMapCacheIT extends AbstractDaemonTest {
   }
 
   @Test
-  public void shouldBeAbleToInstallChronicleMapCacheFactory() {
+  public void shouldBeAbleToInstallChronicleMapCacheFactory() throws Exception {
+    super.beforeTest(testDescription);
     assertThat(persistentCacheFactory).isInstanceOf(ChronicleMapCacheFactory.class);
   }
 
   @Test
   public void shouldCacheNewProject() throws Exception {
+    super.beforeTest(testDescription);
     String newProjectName = name("newProject");
     RestResponse r = adminRestSession.put("/projects/" + newProjectName);
 
@@ -49,6 +67,7 @@ public class ChronicleMapCacheIT extends AbstractDaemonTest {
 
   @Test
   public void shouldCacheNewUser() throws Exception {
+    super.beforeTest(testDescription);
     AccountInput input = new AccountInput();
     input.username = "foo";
 
@@ -56,5 +75,24 @@ public class ChronicleMapCacheIT extends AbstractDaemonTest {
     adminRestSession.put("/accounts/" + input.username, input);
 
     assertThat(accountCache.getByUsername(input.username)).isPresent();
+  }
+
+  @Test(expected = GerritServer.StartupException.class)
+  @UseLocalDisk
+  @GerritConfig(name = "cache.directory", value = "cache")
+  @GerritConfig(name = "cache.diff.diskLimit", value = "1")
+  public void shouldFailToStartGerritWhenDiskLimitCannotBeHonoured() throws Exception {
+    super.beforeTest(testDescription);
+  }
+
+  @Test
+  @UseLocalDisk
+  @GerritConfig(name = "cache.directory", value = "cache")
+  @GerritConfig(name = "cache.persisted_projects.diskLimit", value = "1024000")
+  public void shouldStartGerritWhenDiskLimitIsHigherThanCacheFile() throws Exception {
+    super.beforeTest(testDescription);
+    File cache = gerritSitePath.resolve("cache/persisted_projects_2.dat").toFile();
+    assertThat(cache.exists()).isTrue();
+    assertThat(cache.length()).isLessThan(1_024_000);
   }
 }
