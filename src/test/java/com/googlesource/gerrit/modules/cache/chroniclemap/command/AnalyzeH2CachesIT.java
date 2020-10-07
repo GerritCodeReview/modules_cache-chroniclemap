f7bf4c910e027541bf47a2edb69f4176da65f562
Antonio Barone <syntonyze@gmail.com>
2020-10-07 18:54:08 +0200

    Provide SSH command to analyze H2 caches
    Understanding how to configure cache-chroniclemap might be quite
    difficult without having more information about the size of the caches
    and average values of their keys and values.
    
    Provide an SSH command so that cache-chroniclemap can be installed as a
    plugin before being actually installed as lib module, in order to gather
    information about the existing persisted H2 caches.
    
    Bug: Issue 13475
    Change-Id: I6f6ec28d68d9e5f9ff971a494bd45fd013e16d82

---
+++ src/test/java/com/googlesource/gerrit/modules/cache/chroniclemap/command/AnalyzeH2CachesIT.java
@@ -0,0 +1,129 @@
+// Copyright (C) 2020 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.modules.cache.chroniclemap.command;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableList;
+import com.google.gerrit.acceptance.LightweightPluginDaemonTest;
+import com.google.gerrit.acceptance.TestPlugin;
+import com.google.gerrit.acceptance.UseLocalDisk;
+import com.google.gerrit.acceptance.UseSsh;
+import com.google.gerrit.server.config.SitePaths;
+import com.google.inject.Inject;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import org.junit.Test;
+
+@UseSsh
+@TestPlugin(
+    name = "cache-chroniclemap",
+    sshModule = "com.googlesource.gerrit.modules.cache.chroniclemap.command.SSHCommandModule")
+public class AnalyzeH2CachesIT extends LightweightPluginDaemonTest {
+
+  @Inject private SitePaths sitePaths;
+
+  @Test
+  @UseLocalDisk
+  public void shouldAnalyzeH2Cache() throws Exception {
+    createChange();
+    String cmd = Joiner.on(" ").join("cache-chroniclemap", "analyze-h2-caches");
+
+    String result = adminSshSession.exec(cmd);
+
+    adminSshSession.assertSuccess();
+    assertThat(result)
+        .contains(
+            "[cache \"mergeability\"]\n"
+                + "\tentries = 1\n"
+                + "\tavgKeySize = 150\n"
+                + "\tavgValueSize = 20");
+    assertThat(result).contains("[cache \"diff\"]\n" + "\tentries = 1\n");
+    assertThat(result)
+        .contains(
+            "[cache \"accounts\"]\n"
+                + "\tentries = 4\n"
+                + "\tavgKeySize = 52\n"
+                + "\tavgValueSize = 189");
+    assertThat(result)
+        .contains(
+            "[cache \"diff_summary\"]\n"
+                + "\tentries = 1\n"
+                + "\tavgKeySize = 192\n"
+                + "\tavgValueSize = 178");
+    assertThat(result)
+        .contains(
+            "[cache \"persisted_projects\"]\n"
+                + "\tentries = 3\n"
+                + "\tavgKeySize = 143\n"
+                + "\tavgValueSize = 4150");
+  }
+
+  @Test
+  @UseLocalDisk
+  public void shouldProduceWarningWhenCacheFileIsEmpty() throws Exception {
+    String cmd = Joiner.on(" ").join("cache-chroniclemap", "analyze-h2-caches");
+    List<String> expected =
+        ImmutableList.of(
+            "WARN: Cache diff_intraline is empty, skipping.",
+            "WARN: Cache change_kind is empty, skipping.",
+            "WARN: Cache diff_summary is empty, skipping.",
+            "WARN: Cache diff is empty, skipping.",
+            "WARN: Cache mergeability is empty, skipping.",
+            "WARN: Cache pure_revert is empty, skipping.",
+            "WARN: Cache git_tags is empty, skipping.");
+    String result = adminSshSession.exec(cmd);
+
+    adminSshSession.assertSuccess();
+    assertThat(ImmutableList.copyOf(result.split("\n"))).containsAtLeastElementsIn(expected);
+  }
+
+  @Test
+  @UseLocalDisk
+  public void shouldIgnoreNonH2Files() throws Exception {
+
+    Path cacheDirectory = sitePaths.resolve(cfg.getString("cache", null, "directory"));
+    Files.write(cacheDirectory.resolve("some.dat"), "some_content".getBytes());
+
+    String cmd = Joiner.on(" ").join("cache-chroniclemap", "analyze-h2-caches");
+    List<String> expected =
+        ImmutableList.of(
+            "WARN: Cache diff_intraline is empty, skipping.",
+            "WARN: Cache change_kind is empty, skipping.",
+            "WARN: Cache diff_summary is empty, skipping.",
+            "WARN: Cache diff is empty, skipping.",
+            "WARN: Cache mergeability is empty, skipping.",
+            "WARN: Cache pure_revert is empty, skipping.",
+            "WARN: Cache git_tags is empty, skipping.");
+    String result = adminSshSession.exec(cmd);
+
+    adminSshSession.assertSuccess();
+    assertThat(ImmutableList.copyOf(result.split("\n"))).containsAtLeastElementsIn(expected);
+  }
+
+  @Test
+  @UseLocalDisk
+  public void shouldFailWhenCacheDirectoryDoesNotExists() throws Exception {
+    cfg.setString("cache", null, "directory", "/tmp/non_existing_directory");
+
+    String cmd = Joiner.on(" ").join("cache-chroniclemap", "analyze-h2-caches");
+
+    adminSshSession.exec(cmd);
+    adminSshSession.assertFailure(
+        "fatal: disk cache is configured but doesn't exist: /tmp/non_existing_directory");
+  }
+}
