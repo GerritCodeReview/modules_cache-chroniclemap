a2716d1653e78e83705dbabcccddd7ed5e1e4c2d
Antonio Barone <syntonyze@gmail.com>
2021-02-25 15:16:04 +0100

    Move migration command over HTTP
    Some caches are bound only by the HTTP injector, so in order to migrate
    all persistent caches, the migration command needs to run over HTTP to
    have visibility on all caches.
    
    Crucially this allows to migrate web_sessions too, which is only
    available over HTTP.
    
    Bug: Issue 14086
    Change-Id: I92942cc35b60a37921a3704176a65bace8d25018

--- src/main/java/com/googlesource/gerrit/modules/cache/chroniclemap/H2MigrationEndpoint.java
+++ src/main/java/com/googlesource/gerrit/modules/cache/chroniclemap/H2MigrationEndpoint.java
@@ -11,56 +11,55 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 package com.googlesource.gerrit.modules.cache.chroniclemap;
 
 import static com.googlesource.gerrit.modules.cache.chroniclemap.H2CacheCommand.H2_SUFFIX;
 import static com.googlesource.gerrit.modules.cache.chroniclemap.H2CacheCommand.appendToConfig;
-import static com.googlesource.gerrit.modules.cache.chroniclemap.H2CacheCommand.getCacheDir;
 import static com.googlesource.gerrit.modules.cache.chroniclemap.H2CacheCommand.getStats;
 import static com.googlesource.gerrit.modules.cache.chroniclemap.H2CacheCommand.jdbcUrl;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.flogger.FluentLogger;
-import com.google.gerrit.common.data.GlobalCapability;
-import com.google.gerrit.extensions.annotations.RequiresCapability;
+import com.google.gerrit.extensions.restapi.Response;
+import com.google.gerrit.extensions.restapi.RestApiException;
+import com.google.gerrit.extensions.restapi.RestReadView;
 import com.google.gerrit.metrics.DisabledMetricMaker;
 import com.google.gerrit.server.cache.PersistentCacheDef;
-import com.google.gerrit.server.cache.serialize.CacheSerializer;
-import com.google.gerrit.server.cache.serialize.StringCacheSerializer;
+import com.google.gerrit.server.config.ConfigResource;
 import com.google.gerrit.server.config.GerritServerConfig;
 import com.google.gerrit.server.config.SitePaths;
-import com.google.gerrit.sshd.CommandMetaData;
-import com.google.gerrit.sshd.SshCommand;
-import com.google.inject.Binding;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
-import com.google.inject.Key;
+import com.google.inject.Singleton;
+import com.google.inject.TypeLiteral;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Stream;
+import javax.servlet.http.HttpServletResponse;
 import org.eclipse.jgit.lib.Config;
-import org.eclipse.jgit.lib.TextProgressMonitor;
 import org.h2.Driver;
 import org.kohsuke.args4j.Option;
 
-@RequiresCapability(GlobalCapability.ADMINISTRATE_SERVER)
-@CommandMetaData(name = "migrate-h2-caches", description = "Migrate H2 caches to Chronicle-Map")
-public class MigrateH2Caches extends SshCommand {
+@Singleton
+public class H2MigrationEndpoint implements RestReadView<ConfigResource> {
   protected static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
   private final Injector injector;
   private final ChronicleMapCacheConfig.Factory configFactory;
-
-  protected static int DEFAULT_SIZE_MULTIPLIER = 3;
-  protected static int DEFAULT_MAX_BLOAT_FACTOR = 3;
   private final SitePaths site;
   private final Config gerritConfig;
+
+  public static int DEFAULT_SIZE_MULTIPLIER = 4;
+  public static int DEFAULT_MAX_BLOAT_FACTOR = 4;
 
   @Option(
       name = "--size-multiplier",
@@ -77,10 +76,10 @@ public class MigrateH2Caches extends SshCommand {
   private int maxBloatFactor = DEFAULT_MAX_BLOAT_FACTOR;
 
   @Inject
-  MigrateH2Caches(
+  H2MigrationEndpoint(
+      Injector injector,
       @GerritServerConfig Config cfg,
       SitePaths site,
-      Injector injector,
       ChronicleMapCacheConfig.Factory configFactory) {
     this.injector = injector;
     this.configFactory = configFactory;
@@ -89,16 +88,18 @@ public class MigrateH2Caches extends SshCommand {
   }
 
   @Override
-  protected void run() throws Exception {
-    Optional<Path> cacheDir = getCacheDir(gerritConfig, site);
+  public Response<String> apply(ConfigResource resource) throws Exception {
+    Optional<Path> cacheDir = getCacheDir();
 
     if (!cacheDir.isPresent()) {
-      throw die("Cannot run migration, cache directory is not configured");
+      return Response.withStatusCode(
+          HttpServletResponse.SC_BAD_REQUEST,
+          "Cannot run migration, cache directory is not configured");
     }
 
-    stdout.println("Migrating H2 caches to Chronicle-Map...");
-    stdout.println("* Size multiplier: " + sizeMultiplier);
-    stdout.println("* Max Bloat Factor: " + maxBloatFactor);
+    logger.atInfo().log("Migrating H2 caches to Chronicle-Map...");
+    logger.atInfo().log("* Size multiplier: " + sizeMultiplier);
+    logger.atInfo().log("* Max Bloat Factor: " + maxBloatFactor);
     Set<PersistentCacheDef<?, ?>> cacheDefs = getAllBoundPersistentCacheDefs();
 
     Config outputChronicleMapConfig = new Config();
@@ -117,19 +118,66 @@ public class MigrateH2Caches extends SshCommand {
                       configFactory, cacheDir.get(), in, stats, sizeMultiplier, maxBloatFactor),
                   null,
                   new DisabledMetricMaker());
-          doMigrate(h2CacheFile.get(), in, chronicleMapCache, stats.size());
+          doMigrate(h2CacheFile.get(), in, chronicleMapCache);
           chronicleMapCache.close();
           appendBloatedConfig(outputChronicleMapConfig, stats);
         }
       }
     }
-    stdout.println("Complete!");
-    stdout.println();
-    stdout.println("****************************");
-    stdout.println("** Chronicle-map template **");
-    stdout.println("****************************");
-    stdout.println();
-    stdout.println(outputChronicleMapConfig.toText());
+    logger.atInfo().log("Complete!");
+    return Response.ok(outputChronicleMapConfig.toText());
+  }
+
+  protected Optional<Path> getCacheDir() throws IOException {
+    String name = gerritConfig.getString("cache", null, "directory");
+    if (name == null) {
+      return Optional.empty();
+    }
+    Path loc = site.resolve(name);
+    if (!Files.exists(loc)) {
+      throw new IOException(
+          String.format("disk cache is configured but doesn't exist: %s", loc.toAbsolutePath()));
+    }
+    if (!Files.isReadable(loc)) {
+      throw new IOException(String.format("Can't read from disk cache: %s", loc.toAbsolutePath()));
+    }
+    logger.atFine().log("Enabling disk cache %s", loc.toAbsolutePath());
+    return Optional.of(loc);
+  }
+
+  @VisibleForTesting
+  protected Set<PersistentCacheDef<?, ?>> getAllBoundPersistentCacheDefs() {
+    Set<PersistentCacheDef<?, ?>> cacheDefs = new HashSet<>();
+    Stream.concat(
+            injector.getAllBindings().entrySet().stream(),
+            injector.getParent().getAllBindings().entrySet().stream())
+        .forEach(
+            entry -> {
+              final Class<?> rawType = entry.getKey().getTypeLiteral().getRawType();
+              if ("PersistentCacheDef".equals(rawType.getSimpleName())) {
+                cacheDefs.add((PersistentCacheDef<?, ?>) entry.getValue().getProvider().get());
+              }
+            });
+    return cacheDefs;
+  }
+
+  private Optional<Path> getH2CacheFile(Path cacheDir, String name) {
+    Path h2CacheFile = cacheDir.resolve(String.format("%s.%s", name, H2_SUFFIX));
+    if (Files.exists(h2CacheFile)) {
+      return Optional.of(h2CacheFile);
+    }
+    return Optional.empty();
+  }
+
+  private void appendBloatedConfig(Config config, H2AggregateData stats) {
+    appendToConfig(
+        config,
+        H2AggregateData.create(
+            stats.cacheName(),
+            stats.size() * sizeMultiplier,
+            stats.avgKeySize(),
+            stats.avgValueSize()));
+    config.setLong("cache", stats.cacheName(), "maxBloatFactor", maxBloatFactor);
   }
 
   protected static ChronicleMapCacheConfig makeChronicleMapConfig(
@@ -151,14 +199,8 @@ public class MigrateH2Caches extends SshCommand {
   }
 
   private void doMigrate(
-      Path h2File,
-      PersistentCacheDef<?, ?> in,
-      ChronicleMapCacheImpl<?, ?> chronicleMapCache,
-      long totalEntries)
-      throws UnloggedFailure {
-
-    TextProgressMonitor cacheProgress = new TextProgressMonitor(stdout);
-    cacheProgress.beginTask(String.format("[%s]", in.name()), (int) totalEntries);
+      Path h2File, PersistentCacheDef<?, ?> in, ChronicleMapCacheImpl<?, ?> chronicleMapCache)
+      throws RestApiException {
 
     String url = jdbcUrl(h2File);
     try (Connection conn = Driver.load().connect(url, null)) {
@@ -168,57 +210,27 @@ public class MigrateH2Caches extends SshCommand {
 
       try (ResultSet r = preparedStatement.executeQuery()) {
         while (r.next()) {
-          Object key = in.keySerializer().deserialize(getBytes(r, 1, in.keySerializer()));
-          Object value = in.valueSerializer().deserialize(getBytes(r, 2, in.valueSerializer()));
+          Object key =
+              isStringType(in.keyType())
+                  ? r.getString(1)
+                  : in.keySerializer().deserialize(r.getBytes(1));
+          Object value =
+              isStringType(in.valueType())
+                  ? r.getString(2)
+                  : in.valueSerializer().deserialize(r.getBytes(2));
           Timestamp created = r.getTimestamp(3);
           chronicleMapCache.putUnchecked(key, value, created);
-          cacheProgress.update(1);
         }
       }
 
     } catch (Exception e) {
       String message = String.format("FATAL: error migrating %s H2 cache", in.name());
       logger.atSevere().withCause(e).log(message);
-      stderr.println(message);
-      throw die(e);
+      throw RestApiException.wrap(message, e);
     }
-    cacheProgress.endTask();
   }
 
-  private Set<PersistentCacheDef<?, ?>> getAllBoundPersistentCacheDefs() {
-    Set<PersistentCacheDef<?, ?>> cacheDefs = new HashSet<>();
-    for (Map.Entry<Key<?>, Binding<?>> entry : injector.getParent().getAllBindings().entrySet()) {
-      final Class<?> rawType = entry.getKey().getTypeLiteral().getRawType();
-      if ("PersistentCacheDef".equals(rawType.getSimpleName())) {
-        cacheDefs.add((PersistentCacheDef<?, ?>) entry.getValue().getProvider().get());
-      }
-    }
-    return cacheDefs;
-  }
-
-  private byte[] getBytes(ResultSet r, int columnIndex, CacheSerializer<?> serializer)
-      throws SQLException {
-    return (serializer instanceof StringCacheSerializer)
-        ? r.getString(columnIndex).getBytes()
-        : r.getBytes(columnIndex);
-  }
-
-  private Optional<Path> getH2CacheFile(Path cacheDir, String name) {
-    Path h2CacheFile = cacheDir.resolve(String.format("%s.%s", name, H2_SUFFIX));
-    if (Files.exists(h2CacheFile)) {
-      return Optional.of(h2CacheFile);
-    }
-    return Optional.empty();
-  }
-
-  private void appendBloatedConfig(Config config, H2AggregateData stats) {
-    appendToConfig(
-        config,
-        H2AggregateData.create(
-            stats.cacheName(),
-            stats.size() * sizeMultiplier,
-            stats.avgKeySize(),
-            stats.avgValueSize()));
-    config.setLong("cache", stats.cacheName(), "maxBloatFactor", maxBloatFactor);
+  private boolean isStringType(TypeLiteral<?> typeLiteral) {
+    return typeLiteral.getRawType().getSimpleName().equals("String");
   }
 }
