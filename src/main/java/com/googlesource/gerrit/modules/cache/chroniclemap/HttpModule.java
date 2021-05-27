401fc5c206ba76265e8b0b3f2cd5176af6841549
Antonio Barone <syntonyze@gmail.com>
2021-05-27 11:56:40 +0300

    Chronicle-Map cache tuning
    Bug: Issue 14529
    Change-Id: Icb61b38bcadb67cc172f4a863673c3a8dad3cbd8

--- src/main/java/com/googlesource/gerrit/modules/cache/chroniclemap/HttpModule.java
+++ src/main/java/com/googlesource/gerrit/modules/cache/chroniclemap/HttpModule.java
@@ -15,19 +15,30 @@
 package com.googlesource.gerrit.modules.cache.chroniclemap;
 
 import com.google.gerrit.extensions.config.FactoryModule;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.Key;
 import com.google.inject.servlet.ServletModule;
 
 public class HttpModule extends ServletModule {
+  private final Injector injector;
+
+  @Inject
+  HttpModule(Injector injector) {
+    this.injector = injector;
+  }
 
   @Override
   protected void configureServlets() {
-    install(
-        new FactoryModule() {
-          @Override
-          protected void configure() {
-            factory(ChronicleMapCacheConfig.Factory.class);
-          }
-        });
+    if (injector.getExistingBinding(Key.get(ChronicleMapCacheConfig.Factory.class)) == null) {
+      install(
+          new FactoryModule() {
+            @Override
+            protected void configure() {
+              factory(ChronicleMapCacheConfig.Factory.class);
+            }
+          });
+    }
 
     serve("/migrate").with(H2MigrationServlet.class);
   }
