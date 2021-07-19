bd55745d9fa639b3d648e27134f4f6eb8cee6696
Luca Milanesio <luca.milanesio@gmail.com>
2021-07-19 23:10:13 +0100

    Use existing pre-configured cache config for H2 migration
    When migrating from H2 to ChronicleMap, it may be useful
    to pre-define the target cache configuration beforehand.
    That allows the Gerrit admin to correctly tune a desired
    set of parameters and use them during the cutover from H2
    to ChronicleMap.
    
    Keep the default behaviour (auto-tune from H2) when the
    average key and values sizes are not set, so to allow a
    smooth migration for those Gerrit admins that do not
    need to get the cutover fully automated.
    
    Change-Id: I900cc0ed4a83fde6f77c13eb69c4212251834525

--- src/main/java/com/googlesource/gerrit/modules/cache/chroniclemap/H2MigrationServlet.java
+++ src/main/java/com/googlesource/gerrit/modules/cache/chroniclemap/H2MigrationServlet.java
@@ -178,23 +178,43 @@ public class H2MigrationServlet extends HttpServlet {
     Config outputChronicleMapConfig = new Config();
 
     try {
+      Set<String> gerritConfigCaches = gerritConfig.getSubsections("cache");
       for (PersistentCacheDef<?, ?> in : persistentCacheDefs) {
         Optional<Path> h2CacheFile = getH2CacheFile(cacheDir.get(), in.name());
+        Optional<ChronicleMapCacheConfig> chronicleMapConfig = Optional.empty();
+        Optional<H2AggregateData> stats = Optional.empty();
 
         if (h2CacheFile.isPresent()) {
-          H2AggregateData stats = getStats(h2CacheFile.get());
+          if (gerritConfigCaches.contains(in.name())
+              && gerritConfig.getLong("cache", in.name(), "avgKeySize", 0L) > 0
+              && gerritConfig.getLong("cache", in.name(), "avgValueSize", 0L) > 0
+              && gerritConfig.getLong("cache", in.name(), "maxEntries", 0L) > 0
+              && gerritConfig.getInt("cache", in.name(), "maxBloatFactor", 0) > 0) {
+            chronicleMapConfig =
+                Optional.of(
+                    configFactory.create(
+                        in.name(), null, in.expireAfterWrite(), in.refreshAfterWrite()));
 
-          if (!stats.isEmpty()) {
+          } else {
+            stats = getStats(h2CacheFile.get());
+            chronicleMapConfig =
+                stats.map(
+                    (s) ->
+                        makeChronicleMapConfig(
+                            configFactory, cacheDir.get(), in, s, sizeMultiplier, maxBloatFactor));
+          }
+
+          if (!chronicleMapConfig.isEmpty()) {
             ChronicleMapCacheImpl<?, ?> chronicleMapCache =
                 new ChronicleMapCacheImpl<>(
-                    in,
-                    makeChronicleMapConfig(
-                        configFactory, cacheDir.get(), in, stats, sizeMultiplier, maxBloatFactor),
-                    null,
-                    new DisabledMetricMaker());
+                    in, chronicleMapConfig.get(), null, new DisabledMetricMaker());
             doMigrate(h2CacheFile.get(), in, chronicleMapCache);
             chronicleMapCache.close();
-            appendBloatedConfig(outputChronicleMapConfig, stats, maxBloatFactor, sizeMultiplier);
+
+            stats.ifPresent(
+                (s) ->
+                    appendBloatedConfig(
+                        outputChronicleMapConfig, s, maxBloatFactor, sizeMultiplier));
           }
         }
       }
--- src/test/java/com/googlesource/gerrit/modules/cache/chroniclemap/MigrateH2CachesLocalDiskIT.java
+++ src/test/java/com/googlesource/gerrit/modules/cache/chroniclemap/MigrateH2CachesLocalDiskIT.java
@@ -276,7 +276,8 @@ public class MigrateH2CachesLocalDiskIT extends LightweightPluginDaemonTest {
             cacheDirectory,
             persistentDef,
             H2CacheCommand.getStats(
-                cacheDirectory.resolve(String.format("%s.%s", cacheName, H2_SUFFIX))),
+                    cacheDirectory.resolve(String.format("%s.%s", cacheName, H2_SUFFIX)))
+                .get(),
             DEFAULT_SIZE_MULTIPLIER,
             DEFAULT_MAX_BLOAT_FACTOR);
 
