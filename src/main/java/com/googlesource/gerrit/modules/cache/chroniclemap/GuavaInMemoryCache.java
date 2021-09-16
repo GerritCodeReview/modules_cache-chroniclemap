debbd3184025bf9d668643e27a1bee858c59b9d1
Luca Milanesio <luca.milanesio@gmail.com>
2021-09-16 21:04:17 +0100

    Front CacheChronicleMap with a Guava cache
    The initial implementation of persistent caches using
    ChronicleMap did not use any Guava cache front-end, relying
    on the speed of accessing data on disk and memory.
    
    Unfortunately, the deserialisation of some entities
    (e.g. change_notes) is very expensive in terms of CPU
    and ChronicleMap does not store the binary objects as-is
    but relies on in-memory serialisation of keys and values.
    There are two problems caused by this:
    - delay in reading the cache from memory
    - high system load when accessing the cache
    
    Use the same structure of the H2 persistent cache where
    the loader is delegated to the retrieval and deserialisation
    of the entities from ChronicleMap.
    
    Amend the cache statistics and return the in-memory elements
    and the ones stored on disk as separate values.
    
    Bug: Issue 15036
    Change-Id: I8dd602c20fcd621972b562fc2163fefc7240484a

---
+++ src/main/java/com/googlesource/gerrit/modules/cache/chroniclemap/GuavaInMemoryCache.java
@@ -0,0 +1,94 @@
+// Copyright (C) 2021 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.modules.cache.chroniclemap;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheStats;
+import com.google.common.cache.LoadingCache;
+import com.google.gerrit.common.Nullable;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+
+class GuavaInMemoryCache<K, V> implements InMemoryCache<K, V> {
+  private final Cache<K, TimedValue<V>> guavaCache;
+  private final boolean loadingCache;
+
+  public GuavaInMemoryCache(Cache<K, TimedValue<V>> guavaCache, boolean loadingCache) {
+    this.guavaCache = guavaCache;
+    this.loadingCache = loadingCache;
+  }
+
+  @Override
+  public @Nullable TimedValue<V> getIfPresent(Object key) {
+    return guavaCache.getIfPresent(key);
+  }
+
+  @Override
+  public TimedValue<V> get(K key, Callable<? extends TimedValue<V>> valueLoader) throws Exception {
+    return guavaCache.get(key, valueLoader);
+  }
+
+  @Override
+  public void put(K key, TimedValue<V> value) {
+    guavaCache.put(key, value);
+  }
+
+  @Override
+  public boolean isLoadingCache() {
+    return loadingCache;
+  }
+
+  @Override
+  public TimedValue<V> get(K key) throws ExecutionException {
+    if (loadingCache) {
+      return ((LoadingCache<K, TimedValue<V>>) guavaCache).get(key);
+    }
+
+    TimedValue<V> cachedValue = getIfPresent(key);
+    if (cachedValue != null) {
+      return cachedValue;
+    }
+
+    throw new UnsupportedOperationException(
+        String.format("Could not load value for %s without any loader", key));
+  }
+
+  @Override
+  public void refresh(K key) {
+    if (isLoadingCache()) {
+      ((LoadingCache<K, TimedValue<V>>) guavaCache).refresh(key);
+    }
+  }
+
+  @Override
+  public CacheStats stats() {
+    return guavaCache.stats();
+  }
+
+  @Override
+  public long size() {
+    return guavaCache.size();
+  }
+
+  @Override
+  public void invalidate(Object key) {
+    guavaCache.invalidate(key);
+  }
+
+  @Override
+  public void invalidateAll() {
+    guavaCache.invalidateAll();
+  }
+}
