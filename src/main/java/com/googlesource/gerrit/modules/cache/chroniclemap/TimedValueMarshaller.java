9381a3d71e0da13f84461c7b1be39c2ccc90b7c2
Jacek Centkowski <geminica.programs@gmail.com>
2022-07-13 08:18:51 +0200

    Add read/write metrics to TimedValueMarshaller
    The following metrics were added:
    * cache/chroniclemap/store_serialize_latency_<cache-name> the latency of
      serializing entries in chronicle-map store
    * cache/chroniclemap/store_deserialize_latency_<cache-name> the latency
      of deserializing entries in chronicle-map store
    
    Notes:
    * metrics are not serializable hence as such they have to be transient
    * in tests we are close and re-open store multiple times and in such
      case 'readResolve' method is used instead of constructor and in such
      case metrics object can be 'null' (is not serialized) therefore it
      gets defaulted to 'DisabledMetricMaker'. That shouldn't be case for
      production environment
    
    Change-Id: I195d83d4ebf351ad82925130d4e0c77231c3ec07

--- src/main/java/com/googlesource/gerrit/modules/cache/chroniclemap/TimedValueMarshaller.java
+++ src/main/java/com/googlesource/gerrit/modules/cache/chroniclemap/TimedValueMarshaller.java
@@ -13,7 +13,14 @@
 // limitations under the License.
 package com.googlesource.gerrit.modules.cache.chroniclemap;
 
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.metrics.Description;
+import com.google.gerrit.metrics.Description.Units;
+import com.google.gerrit.metrics.DisabledMetricMaker;
+import com.google.gerrit.metrics.MetricMaker;
+import com.google.gerrit.metrics.Timer0;
 import java.nio.ByteBuffer;
+import java.util.Optional;
 import net.openhft.chronicle.bytes.Bytes;
 import net.openhft.chronicle.core.util.ReadResolvable;
 import net.openhft.chronicle.hash.serialization.BytesReader;
@@ -24,67 +31,118 @@ public class TimedValueMarshaller<V>
         BytesReader<TimedValue<V>>,
         ReadResolvable<TimedValueMarshaller<V>> {
 
-  private final String name;
+  private static class Metrics {
+    private final Timer0 deserializeLatency;
+    private final Timer0 serializeLatency;
 
-  TimedValueMarshaller(String name) {
+    private Metrics(MetricMaker metricMaker, String cacheName) {
+      String sanitizedName = metricMaker.sanitizeMetricName(cacheName);
+
+      deserializeLatency =
+          metricMaker.newTimer(
+              "cache/chroniclemap/store_deserialize_latency_" + sanitizedName,
+              new Description(
+                      String.format(
+                          "The latency of deserializing entries from chronicle-map store for %s"
+                              + " cache",
+                          cacheName))
+                  .setCumulative()
+                  .setUnit(Units.NANOSECONDS));
+
+      serializeLatency =
+          metricMaker.newTimer(
+              "cache/chroniclemap/store_serialize_latency_" + sanitizedName,
+              new Description(
+                      String.format(
+                          "The latency of serializing entries to chronicle-map store for %s cache",
+                          cacheName))
+                  .setCumulative()
+                  .setUnit(Units.NANOSECONDS));
+    }
+  }
+
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+  private final String name;
+  private final Metrics metrics;
+
+  TimedValueMarshaller(MetricMaker metricMaker, String name) {
+    this(new Metrics(metricMaker, name), name);
+  }
+
+  private TimedValueMarshaller(Metrics metrics, String name) {
+    this.metrics = metrics;
     this.name = name;
   }
 
   @Override
   public TimedValueMarshaller<V> readResolve() {
-    return new TimedValueMarshaller<>(name);
+    Metrics toCopy =
+        Optional.ofNullable(metrics)
+            .orElseGet(
+                () -> {
+                  logger.atSevere().log(
+                      "DisabledMetricMaker is used to create metrics for '%s' cache. That shouldn't happen outside the tests.",
+                      name);
+                  return new Metrics(new DisabledMetricMaker(), name);
+                });
+    return new TimedValueMarshaller<>(toCopy, name);
   }
 
   @SuppressWarnings({"rawtypes", "unchecked"})
   @Override
   public TimedValue<V> read(Bytes in, TimedValue<V> using) {
-    long initialPosition = in.readPosition();
+    try (Timer0.Context timer = metrics.deserializeLatency.start()) {
+      long initialPosition = in.readPosition();
 
-    // Deserialize the creation timestamp (first 8 bytes)
-    byte[] serializedLong = new byte[Long.BYTES];
-    in.read(serializedLong, 0, Long.BYTES);
-    ByteBuffer buffer = ByteBuffer.wrap(serializedLong);
-    long created = buffer.getLong(0);
-    in.readPosition(initialPosition + Long.BYTES);
+      // Deserialize the creation timestamp (first 8 bytes)
+      byte[] serializedLong = new byte[Long.BYTES];
+      in.read(serializedLong, 0, Long.BYTES);
+      ByteBuffer buffer = ByteBuffer.wrap(serializedLong);
+      long created = buffer.getLong(0);
+      in.readPosition(initialPosition + Long.BYTES);
 
-    // Deserialize the length of the serialized value (second 8 bytes)
-    byte[] serializedInt = new byte[Integer.BYTES];
-    in.read(serializedInt, 0, Integer.BYTES);
-    ByteBuffer buffer2 = ByteBuffer.wrap(serializedInt);
-    int vLength = buffer2.getInt(0);
-    in.readPosition(initialPosition + Long.BYTES + Integer.BYTES);
+      // Deserialize the length of the serialized value (second 8 bytes)
+      byte[] serializedInt = new byte[Integer.BYTES];
+      in.read(serializedInt, 0, Integer.BYTES);
+      ByteBuffer buffer2 = ByteBuffer.wrap(serializedInt);
+      int vLength = buffer2.getInt(0);
+      in.readPosition(initialPosition + Long.BYTES + Integer.BYTES);
 
-    // Deserialize object V (remaining bytes)
-    byte[] serializedV = new byte[vLength];
-    in.read(serializedV, 0, vLength);
-    V v = (V) CacheSerializers.getValueSerializer(name).deserialize(serializedV);
+      // Deserialize object V (remaining bytes)
+      byte[] serializedV = new byte[vLength];
+      in.read(serializedV, 0, vLength);
+      V v = (V) CacheSerializers.getValueSerializer(name).deserialize(serializedV);
 
-    using = new TimedValue<>(v, created);
+      using = new TimedValue<>(v, created);
 
-    return using;
+      return using;
+    }
   }
 
   @SuppressWarnings("rawtypes")
   @Override
   public void write(Bytes out, TimedValue<V> toWrite) {
-    byte[] serialized = CacheSerializers.getValueSerializer(name).serialize(toWrite.getValue());
+    try (Timer0.Context timer = metrics.serializeLatency.start()) {
+      byte[] serialized = CacheSerializers.getValueSerializer(name).serialize(toWrite.getValue());
 
-    // Serialize as follows:
-    // created | length of serialized V | serialized value V
-    // 8 bytes |       4 bytes          | serialized_length bytes
+      // Serialize as follows:
+      // created | length of serialized V | serialized value V
+      // 8 bytes |       4 bytes          | serialized_length bytes
 
-    int capacity = Long.BYTES + Integer.BYTES + serialized.length;
-    ByteBuffer buffer = ByteBuffer.allocate(capacity);
+      int capacity = Long.BYTES + Integer.BYTES + serialized.length;
+      ByteBuffer buffer = ByteBuffer.allocate(capacity);
 
-    long timestamp = toWrite.getCreated();
-    buffer.putLong(0, timestamp);
+      long timestamp = toWrite.getCreated();
+      buffer.putLong(0, timestamp);
 
-    buffer.position(Long.BYTES);
-    buffer.putInt(serialized.length);
+      buffer.position(Long.BYTES);
+      buffer.putInt(serialized.length);
 
-    buffer.position(Long.BYTES + Integer.BYTES);
-    buffer.put(serialized);
+      buffer.position(Long.BYTES + Integer.BYTES);
+      buffer.put(serialized);
 
-    out.write(buffer.array());
+      out.write(buffer.array());
+    }
   }
 }
