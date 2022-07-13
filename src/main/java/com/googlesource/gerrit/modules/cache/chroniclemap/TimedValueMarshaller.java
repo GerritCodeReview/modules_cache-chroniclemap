ebe2250fd83955dfb4bfd0c3e0e1e202a48e1485
Jacek Centkowski <geminica.programs@gmail.com>
2022-07-13 08:48:27 +0200

    TimeValueMarshaller: use higher level API for (de)serialization
    At present each read/write call results in buffers being created at
    least once for write but more for read. Use higher level API in order to
    avoid buffer creation and leave it up to the implementation to optimize
    on it.
    
    Change-Id: Ifb194e49b9bace0cf4d81ef3af3d6a64f0564dbd

--- src/main/java/com/googlesource/gerrit/modules/cache/chroniclemap/TimedValueMarshaller.java
+++ src/main/java/com/googlesource/gerrit/modules/cache/chroniclemap/TimedValueMarshaller.java
@@ -14,7 +14,6 @@
 package com.googlesource.gerrit.modules.cache.chroniclemap;
 
 import com.google.gerrit.server.cache.serialize.CacheSerializer;
-import java.nio.ByteBuffer;
 import net.openhft.chronicle.bytes.Bytes;
 import net.openhft.chronicle.core.util.ReadResolvable;
 import net.openhft.chronicle.hash.serialization.BytesReader;
@@ -41,30 +40,18 @@ public class TimedValueMarshaller<V>
   @SuppressWarnings("rawtypes")
   @Override
   public TimedValue<V> read(Bytes in, TimedValue<V> using) {
-    long initialPosition = in.readPosition();
-
     // Deserialize the creation timestamp (first 8 bytes)
-    byte[] serializedLong = new byte[Long.BYTES];
-    in.read(serializedLong, 0, Long.BYTES);
-    ByteBuffer buffer = ByteBuffer.wrap(serializedLong);
-    long created = buffer.getLong(0);
-    in.readPosition(initialPosition + Long.BYTES);
+    long created = in.readLong();
 
-    // Deserialize the length of the serialized value (second 8 bytes)
-    byte[] serializedInt = new byte[Integer.BYTES];
-    in.read(serializedInt, 0, Integer.BYTES);
-    ByteBuffer buffer2 = ByteBuffer.wrap(serializedInt);
-    int vLength = buffer2.getInt(0);
-    in.readPosition(initialPosition + Long.BYTES + Integer.BYTES);
+    // Deserialize the length of the serialized value (second 4 bytes)
+    int vLength = (int) in.readUnsignedInt();
 
     // Deserialize object V (remaining bytes)
     byte[] serializedV = new byte[vLength];
     in.read(serializedV, 0, vLength);
     V v = cacheSerializer.deserialize(serializedV);
 
-    using = new TimedValue<>(v, created);
-
-    return using;
+    return new TimedValue<>(v, created);
   }
 
   @SuppressWarnings("rawtypes")
@@ -75,19 +62,8 @@ public class TimedValueMarshaller<V>
     // Serialize as follows:
     // created | length of serialized V | serialized value V
     // 8 bytes |       4 bytes          | serialized_length bytes
-
-    int capacity = Long.BYTES + Integer.BYTES + serialized.length;
-    ByteBuffer buffer = ByteBuffer.allocate(capacity);
-
-    long timestamp = toWrite.getCreated();
-    buffer.putLong(0, timestamp);
-
-    buffer.position(Long.BYTES);
-    buffer.putInt(serialized.length);
-
-    buffer.position(Long.BYTES + Integer.BYTES);
-    buffer.put(serialized);
-
-    out.write(buffer.array());
+    out.writeLong(toWrite.getCreated());
+    out.writeUnsignedInt(serialized.length);
+    out.write(serialized);
   }
 }
