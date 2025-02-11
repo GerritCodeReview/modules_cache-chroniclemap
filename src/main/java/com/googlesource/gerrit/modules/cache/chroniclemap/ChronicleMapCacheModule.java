67264c4c2f4032d6da57499cb7f51836da099b09
Luca Milanesio <luca.milanesio@gmail.com>
2025-02-11 22:43:47 +0000

    Adapt to the new PersistentCacheBaseFactory on master
    The new PersistentCacheBaseFactory replaces the SitePaths
    with the H2-specific @CacheDir Path, which should be included
    from the H2-backend of Gerrit persisted caches.
    
    Change-Id: I0bdc792f470406cdc4ed4c1f90495a7907fb34b5

--- src/main/java/com/googlesource/gerrit/modules/cache/chroniclemap/ChronicleMapCacheModule.java
+++ src/main/java/com/googlesource/gerrit/modules/cache/chroniclemap/ChronicleMapCacheModule.java
@@ -13,13 +13,25 @@
 // limitations under the License.
 package com.googlesource.gerrit.modules.cache.chroniclemap;
 
+import com.google.common.flogger.FluentLogger;
+import com.google.gerrit.common.Nullable;
 import com.google.gerrit.lifecycle.LifecycleModule;
 import com.google.gerrit.server.ModuleImpl;
 import com.google.gerrit.server.cache.CacheModule;
 import com.google.gerrit.server.cache.PersistentCacheFactory;
+import com.google.gerrit.server.config.GerritServerConfig;
+import com.google.gerrit.server.config.SitePaths;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.eclipse.jgit.lib.Config;
 
 @ModuleImpl(name = CacheModule.PERSISTENT_MODULE)
 public class ChronicleMapCacheModule extends LifecycleModule {
+
+  private static final FluentLogger logger = FluentLogger.forEnclosingClass();
 
   @Override
   protected void configure() {
@@ -27,5 +39,31 @@ public class ChronicleMapCacheModule extends LifecycleModule {
     bind(PersistentCacheFactory.class).to(ChronicleMapCacheFactory.class);
     listener().to(ChronicleMapCacheFactory.class);
     bind(CachesWithoutChronicleMapConfigMetric.class).asEagerSingleton();
+  }
+
+  @Provides
+  @Singleton
+  @Nullable
+  @ChronicleMapDir
+  Path getChronicleMapDir(SitePaths site, @GerritServerConfig Config config) {
+    String name = config.getString("cache", null, "directory");
+    if (name == null) {
+      return null;
+    }
+    Path loc = site.resolve(name);
+    if (!Files.exists(loc)) {
+      try {
+        Files.createDirectories(loc);
+      } catch (IOException e) {
+        logger.atWarning().log("Can't create disk cache: %s", loc.toAbsolutePath());
+        return null;
+      }
+    }
+    if (!Files.isWritable(loc)) {
+      logger.atWarning().log("Can't write to disk cache: %s", loc.toAbsolutePath());
+      return null;
+    }
+    logger.atInfo().log("Enabling disk cache %s", loc.toAbsolutePath());
+    return loc;
   }
 }
